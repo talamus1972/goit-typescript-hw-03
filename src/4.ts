be2f531044b6cb882773de7ab9d0a5f623a3c60c
talamus1972 <talamus1972@gmail.com>
2024-04-13 18:34:28 +0300

    3

--- src/4.ts
+++ src/4.ts
@@ -43,17 +43,18 @@ abstract class House {
   }
 }
 
-class MyHouse extends House {
-  constructor(key: Key) {
-    super(key);
-  }
 
-  openDoor(key: Key): void {
+class MyHouse extends House {
+openDoor(key: Key): void {
     if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
     } else {
     }
   }
 }
+
+    
+ 
+
 
 
